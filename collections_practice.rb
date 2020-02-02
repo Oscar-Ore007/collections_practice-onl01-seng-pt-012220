5fd9c32042c2766a916559b61ba9d0633fc53a5f
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-02 02:18:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,9 +27,9 @@ def reverse_array(array)
   array.reverse 
 end 
 
-def kesha_maker(array) 
-  array.collection do |element| 
-    element[2] = $ 
-    element 
- end 
-end 
+def kesha_maker (array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
